94301763bccd01ee906e78090cc7bf5ff93acd3e
Mohamed Dah Agove <agwfmhmd@gmail.com>
2026-09-11 13:09:47 +0000

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='teyssir-erp-v2';
+const SHELL=['/','/index.html','/manifest.json','/icon-192.png','/icon-512.png','/logo.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(r=>{if(r.ok){const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy))}return r}).catch(()=>caches.match(event.request).then(r=>r||caches.match('/index.html'))))});
